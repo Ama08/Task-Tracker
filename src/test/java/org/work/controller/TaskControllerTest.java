31be32e19782124ad62d22ac6fe48c5c6cc74938
Ama08 <amasasini@gmail.com>
2025-04-11 19:31:48 +0100

    tests

--- src/test/java/org/work/controller/TaskControllerTest.java
+++ src/test/java/org/work/controller/TaskControllerTest.java
@@ -4,11 +4,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.server.ResponseStatusException;
 import org.work.model.Task;
 import org.work.service.TaskService;
 
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(TaskController.class)
 public class TaskControllerTest {
@@ -28,7 +33,18 @@ public class TaskControllerTest {
 
         when(taskService.updateTaskStatus(1L, "Done")).thenReturn(task);
 
-        mockMvc.perform("/tasks/1/status")
-                .param("status", "Done")
+        mockMvc.perform(put("/tasks/1/status")
+                .param("status", "Done"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("Done"));
+    }
+    @Test
+    void shouldReturn404_whenTaskNotFound() throws Exception {
+        when(taskService.updateTaskStatus(99L, "Done"))
+                .thenThrow(new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        mockMvc.perform(put("/tasks/99/status")
+                        .param("status", "Done"))
+                .andExpect(status().isNotFound());
     }
 }
